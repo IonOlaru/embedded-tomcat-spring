0683dea7ffd74432ebbb14a97fd5ae0f5658783d
Ion Olaru <ion.olaru@gmail.com>
2014-02-10 21:50:57 -0500

    adding jersey

---
+++ src/main/java/rest/JSONService.java
@@ -0,0 +1,24 @@
+package rest;
+
+import main.Main;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * @author Ion Olaru
+ *         Date: 2014/02/10 - 21:18
+ */
+@Path("/all")
+public class JSONService {
+
+        @GET
+    	@Path("/get")
+    	@Produces(MediaType.APPLICATION_JSON)
+    	public List getTrackInJSON() {
+            return Main.getTMService().loadAll();
+        }
+}
